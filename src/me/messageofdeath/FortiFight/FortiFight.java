3ede9922ad8ef3dc2a2dc8035f348a655fbc7062
messageofDEATH <messageofdeath@live.com>
2012-11-18 18:48:31 -0600

    Added a log method and a few variables

--- src/me/messageofdeath/FortiFight/FortiFight.java
+++ src/me/messageofdeath/FortiFight/FortiFight.java
@@ -1,11 +1,27 @@
 package me.messageofdeath.FortiFight;
 
+import java.util.logging.Level;
+
+import me.messageofdeath.FortiFight.Listeners.playerListener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FortiFight extends JavaPlugin {
 	
+	public static String prefix;
+	public static PluginDescriptionFile file;
+	
 	@Override
 	public void onEnable() {
-		
+		file = getDescription();
+		prefix = "[" + file.getName() + "] " +  file.getVersion() + ": ";
+		getServer().getPluginManager().registerEvents(new playerListener(), this);
+		log(Level.INFO, "is now enabled!");
+	}
+	
+	public static void log(Level level, String log) {
+		Bukkit.getLogger().log(level, prefix + log);
 	}
 }
